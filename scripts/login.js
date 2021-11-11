7dfcd039453e546e3e9009f8d1a459067d352a27
giani-rossi <74798952+giani-rossi@users.noreply.github.com>
2021-11-11 11:44:06 -0300

    Add files via upload

---
+++ scripts/login.js
@@ -0,0 +1,21 @@
+
+$('#btnIngresar').click(function (){
+
+
+const nombre = $('#nombre').val();
+console.log(nombre);
+localStorage.setItem('nombre',nombre);
+const email = $('#email').val();
+localStorage.setItem('email',email);
+
+})
+
+const value = localStorage.getItem('nombre')
+const value_email = localStorage.getItem('email')
+console.log(value);
+
+let info = []
+info.push(value)
+info.push(value_email)
+console.log(info);
+
